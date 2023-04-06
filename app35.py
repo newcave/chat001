2289785e4ad60f44e7ce0e65d498f3e5090c6623
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-04-06 17:16:51 +0900

    Update app35.py

--- app35.py
+++ app35.py
@@ -1,49 +1,44 @@
-# import openai
-# import streamlit as st
-# from streamlit_chat import message
-# import os 
-# from dotenv import load_dotenv
-
-
-
-# # load_dotenv('api_key.env')
-# openai.api_key = os.environ.get('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')
-
-# def generate_response(prompt):
-#     completion=openai.Completion.create(
-#         engine='text-davinci-003',
-#         prompt=prompt,
-#         max_tokens=1024,
-#         n=1,
-#         stop=None,
-#         temperature=0.6,
-#     )
-#     message=completion.choices[0].text
-#     return message
-
-
-# st.title("ChatGPT-like Web App")
-# #storing the chat
-# if 'generated' not in st.session_state:
-#     st.session_state['generated'] = []
-# if 'past' not in st.session_state:
-#     st.session_state['past'] = []
-# user_input=st.text_input("You:",key='input')
-# if user_input:
-#     output=generate_response(user_input)
-#     #store the output
-#     st.session_state['past'].append(user_input)
-#     st.session_state['generated'].append(output)
-# if st.session_state['generated']:
-#     for i in range(len(st.session_state['generated'])-1, -1, -1):
-#         message(st.session_state["generated"][i], key=str(i))
-#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-
-
-#################################################################################################
-
+import openai
 import streamlit as st
 from streamlit_chat import message
+import os 
+from dotenv import load_dotenv
 
-message("My message") 
-message("Hello bot!", is_user=True)  # align's the message to the right
+
+
+# load_dotenv('api_key.env')
+openai.api_key = os.environ.get('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')
+
+def generate_response(prompt):
+    completion=openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.6,
+    )
+    message=completion.choices[0].text
+    return message
+
+
+st.title("ChatGPT-like Web App")
+#storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+user_input=st.text_input("You:",key='input')
+if user_input:
+    output=generate_response(user_input)
+    #store the output
+    st.session_state['past'].append(user_input)
+    st.session_state['generated'].append(output)
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+
+################################################################################################
+
