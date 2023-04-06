96ce8ee1ffa47044d952225a70f6b371ce8aeae0
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-04-06 17:40:20 +0900

    Update app35.py

--- app35.py
+++ app35.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 
 # load_dotenv('api_key.env')
-openai.api_key = os.environ.get('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')
+openai.api_key = os.environ.get('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')
 
 def generate_response(prompt):
     completion=openai.Completion.create(
