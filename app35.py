e58c9d355364644497b01b4bedebf87da28481d5
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-04-06 11:22:49 +0900

    Update app35.py

--- app35.py
+++ app35.py
@@ -3,20 +3,6 @@ import streamlit as st
 from streamlit_chat import message
 import os 
 from dotenv import load_dotenv
-load_dotenv('api_key.env')
-openai.api_key = os.environ.get('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')
-def generate_response(prompt):
-    completion=openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.6,
-    )
-    message=completion.choices[0].text
-    return message
-
 
 st.title("ChatGPT-like Web App")
 #storing the chat
@@ -34,3 +20,19 @@ if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+
+
+load_dotenv('api_key.env')
+openai.api_key = os.environ.get('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')
+def generate_response(prompt):
+    completion=openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.6,
+    )
+    message=completion.choices[0].text
+    return message
